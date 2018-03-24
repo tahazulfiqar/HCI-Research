f2e610fb18aec32ad2e72ff391363d0ce545e9b9
Gabriel <gab.bussieres@gmail.com>
2018-03-24 16:54:55 -0400

    Move preview with cursor

--- p2b/src/components/App.js
+++ p2b/src/components/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     return (
       <div href="#" onClick={this.toggle}>
         {this.props.children}
-        {this.state.modal ? (
+        {this.state.modal && !this.props.app.isEmpty ? (
           <div
             className={this.props.className}
             style={{
--- p2b/src/components/Preview.js
+++ p2b/src/components/Preview.js
@@ -1,18 +1,68 @@
 import React, { Component } from "react";
+import { NodeGroup } from "react-move";
 import importAll from "../utils/photoImport";
 
 const images = importAll(require.context("../images/dataset", false, /\.jpg/));
 
 class Preview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 650, y: 450 };
+  }
+
+  componentDidMount() {
+    window.addEventListener("mousemove", this.handleMouseMove);
+  }
+
+  _onMouseMove(e) {
+    this.setState({ x: e.screenX, y: e.screenY });
+  }
+
   render() {
     console.log(this.props);
     const filename = this.props.preview.selected + ".jpg";
-    return this.props.preview.isEmpty ? (
-      <h2 style={{ paddingTop: "90px" }}>No photos left</h2>
-    ) : (
-      <div>
-        <img src={images[filename]} alt={filename} height="230" />
-      </div>
+    const { x, y } = this.state;
+
+    return (
+      <NodeGroup
+        data={{
+          x: this.state.x,
+          y: this.state.y
+        }}
+        keyAccessor={d => d.key}
+        start={data => {
+          return { x: data.x, y: data.y };
+        }}
+        update={(data, index) => {
+          return {
+            x: [data.x],
+            y: [data.y],
+            timing: {
+              delay: index * 120,
+              duration: 1300
+            }
+          };
+        }}
+      >
+        {data =>
+          this.props.preview.isEmpty ? (
+            <h2 style={{ paddingTop: "90px" }}>No photos left</h2>
+          ) : (
+            <div
+              onMouseMove={this._onMouseMove.bind(this)}
+              style={{
+                WebkitTransform: `translate3d(${x - 650}px, ${y - 450}px, 0)`,
+                transform: `translate3d(${x - 650}px, ${y - 450}px, 0)`
+              }}
+            >
+              <h1>
+                {x} {y}
+              </h1>
+              <img src={images[filename]} alt={filename} height="230" />
+            </div>
+          )
+        }
+      </NodeGroup>
     );
   }
 }
