f4ed626bbdd7847608f8d8b4f7f79e764ebd37c8
Gabriel <gab.bussieres@gmail.com>
2018-03-24 20:40:46 -0400

    Add log for timestamps

--- p2b/src/reducers/classifications.js
+++ p2b/src/reducers/classifications.js
@@ -1,6 +1,7 @@
 import * as ActionTypes from "../actions/classify";
 
 const InitialState = {
+  log: [],
   left: { folderName: "Text", photos: [] },
   up: { folderName: "Faces", photos: [] },
   right: { folderName: "Other", photos: [] }
@@ -11,33 +12,36 @@ export function classifications(state = InitialState, action) {
     case ActionTypes.CLASSIFY_LEFT: {
       return {
         ...state,
+        log: state.log.concat([
+          { id: action.photo, timestamp: action.timestamp }
+        ]),
         left: {
           ...state.left,
-          photos: state.left.photos.concat([
-            { id: action.photo, timestamp: action.timestamp }
-          ])
+          photos: state.left.photos.concat([action.photo])
         }
       };
     }
     case ActionTypes.CLASSIFY_UP: {
       return {
         ...state,
+        log: state.log.concat([
+          { id: action.photo, timestamp: action.timestamp }
+        ]),
         up: {
           ...state.up,
-          photos: state.up.photos.concat([
-            { id: action.photo, timestamp: action.timestamp }
-          ])
+          photos: state.up.photos.concat([action.photo])
         }
       };
     }
     case ActionTypes.CLASSIFY_RIGHT: {
       return {
         ...state,
+        log: state.log.concat([
+          { id: action.photo, timestamp: action.timestamp }
+        ]),
         right: {
           ...state.right,
-          photos: state.right.photos.concat([
-            { id: action.photo, timestamp: action.timestamp }
-          ])
+          photos: state.right.photos.concat([action.photo])
         }
       };
     }
